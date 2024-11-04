a214f09939b9aea8b88be85e9babe3ff1652b150
flowma.24 <pnquyen.nvla2.2019@gmail.com>
2024-11-04 08:20:52 +0700

    feat: create reccomend details ui

--- src/components/AttractionItem.tsx
+++ src/components/AttractionItem.tsx
@@ -1,24 +1,24 @@
 import { CardContent } from '@/components/ui/card'
 import React from 'react'
 
-interface ReccomendAttractionItemProps {
+interface AttractionItemProps {
   imageUrl: string
-  continent: string
+  continent: string | null
   country: string
 }
 
-const ReccomendAttractionItem: React.FC<ReccomendAttractionItemProps> = ({
-  ...props
-}) => {
+const AttractionItem: React.FC<AttractionItemProps> = ({ ...props }) => {
   return (
     <CardContent className="flex-col font-Urbanist">
       <img className="rounded-3xl" src={props.imageUrl} alt={props.country} />
       <div className="py-1">
-        <p className="text-[#5B5B5B] text-sm">{props.continent}</p>
+        {props.continent && (
+          <p className="text-[#5B5B5B] text-sm">{props.continent}</p>
+        )}
         <p className="text-[#000000] text-base font-bold">{props.country}</p>
       </div>
     </CardContent>
   )
 }
 
-export default ReccomendAttractionItem
+export default AttractionItem
