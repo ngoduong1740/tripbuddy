6bc7a78a9ea23677ba04730e2218f93c4c081a21
bngbtran <tranbnb.2004@gmail.com>
2024-11-07 00:07:50 +0700

    feat: implement login

--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -1,6 +1,42 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    try {
+      const response = await axios.post(
+        'http://ec2-18-140-33-153.ap-southeast-1.compute.amazonaws.com/api/Authen/login',
+        {
+          email: email,
+          password: password,
+        }
+      )
+
+      if (response.status === 200) {
+        navigate('/')
+      } else if (response.status === 500) {
+        alert('Wrong email or password. Please try again !!!')
+      } else {
+        alert('Login failed.')
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error('Error during login:', error.message)
+      } else {
+        console.error('Unexpected error:', error)
+      }
+      alert('Login failed. Please try again.')
+    }
+  }
+
   return (
     <div className="flex h-screen">
       <div
@@ -13,7 +49,7 @@ const Login = () => {
 
       <div className="w-1/2 flex flex-col justify-center items-center bg-white p-10">
         <h2 className="text-3xl font-bold text-[#397D54] mb-4 font-urbanist">
-          Welcome Back, Jonathan 👋
+          Welcome Back,
         </h2>
         <p className="text-gray-500 mb-6 font-urbanist">
           Continue with Google or Enter Login Details
@@ -37,13 +73,15 @@ const Login = () => {
           <hr className="flex-grow border-gray-300" />
         </div>
 
-        <form className="w-4/5">
+        <form className="w-4/5" onSubmit={handleLogin}>
           <div className="mb-4">
             <input
               type="email"
               id="email"
               className="mt-1 p-3 w-full border-0 border-b-2 border-b-gray-300 focus:border-[#397D54] focus:ring-0 placeholder-[#5B5B5B] font-urbanist"
               placeholder="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
 
@@ -53,6 +91,8 @@ const Login = () => {
               id="password"
               className="mt-1 p-3 w-full border-0 border-b-2 border-b-gray-300 focus:border-[#397D54] focus:ring-0 placeholder-[#5B5B5B] font-urbanist"
               placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
 
             <button
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,46 @@
-import axios from 'axios'
+import axios, { AxiosInstance, InternalAxiosRequestConfig } from 'axios'
 
-const api = axios.create({
+export interface ApiError {
+  status: number
+  message: string
+}
+
+const api: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 })
+
+api.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const customError: ApiError = {
+      status: error.response?.status || 500,
+      message: error.response?.data?.message || 'Something went wrong',
+    }
+
+    if (customError.status === 401) {
+      localStorage.removeItem('token')
+      window.location.href = '/login'
+    }
+
+    return Promise.reject(customError)
+  }
+)
 
 export default api
